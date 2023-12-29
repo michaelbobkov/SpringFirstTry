dfa5689a2768d0a3c50524f059cbd56da0e34967
michaelbobkov <michaelbobkov1@gmail.com>
2023-12-29 20:26:34 +0100

    first commit

---
+++ src/test/java/com/example/springfirsttry/SpringFIrstTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfirsttry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFIrstTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
